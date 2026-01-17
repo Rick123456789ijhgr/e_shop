faaebfa0cf27da70eeaf7cd5c51b6e1aab74b9ab
Rick <rick941004@gmail.com>
2026-01-17 17:05:22 +0800

    實現輪播圖

--- lib/components/Home/HomeSlider.dart
+++ lib/components/Home/HomeSlider.dart
@@ -1,20 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_shop/viewmodels/home.dart';
 import 'package:flutter/material.dart';
 
 class HomeSlider extends StatefulWidget {
-  const HomeSlider({super.key});
+  final List<BannerItem> bannerList;
+  const HomeSlider({super.key, required this.bannerList});
 
   @override
   State<HomeSlider> createState() => _HomeSliderState();
 }
 
 class _HomeSliderState extends State<HomeSlider> {
+  Widget _getSlider() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (int index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl,
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 5),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text("輪播圖", style: TextStyle(fontSize: 20, color: Colors.black)),
-    );
+    return Stack(children: [_getSlider()]);
+    // return Container(
+    //   height: 300,
+    //   color: Colors.blue,
+    //   alignment: Alignment.center,
+    //   child: Text("輪播圖", style: TextStyle(fontSize: 20, color: Colors.black)),
+    // );
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -3,6 +3,7 @@ import 'package:e_shop/components/Home/HomeHot.dart';
 import 'package:e_shop/components/Home/HomeMoreList.dart';
 import 'package:e_shop/components/Home/HomeRecommendation.dart';
 import 'package:e_shop/components/Home/HomeSlider.dart';
+import 'package:e_shop/viewmodels/home.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -13,9 +14,23 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: "1",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg",
+    ),
+    BannerItem(
+      id: "2",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.jpg",
+    ),
+    BannerItem(
+      id: "3",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/3.jpg",
+    ),
+  ];
   List<Widget> _getScrollChildren() {
     return [
-      SliverToBoxAdapter(child: HomeSlider()),
+      SliverToBoxAdapter(child: HomeSlider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       SliverToBoxAdapter(child: HomeCategory()),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
